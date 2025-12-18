578710c9caa7d0c17959d5d68ad8544520536376
Mox TALES <fahriahmadhambalii@gmail.com>
2025-12-18 16:31:29 +0700

    Initial commit: Kriteria 3 Workflow CI

---
+++ MLProject/modelling.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+# PENTING: Sesuaikan path dataset agar terbaca oleh MLProject
+dataset_path = "dataset_preprocessing/cleaned_diabetes.csv"
+df = pd.read_csv(dataset_path)
+
+with mlflow.start_run():
+    # ... (kode pelatihan model Anda)
+    mlflow.sklearn.autolog()
+    print("Re-training model selesai!")
